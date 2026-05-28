ad2ec6fe3f249684cc83d0d8fc568e99377db456
Mariam Hagras <mariamhagras99@gmail.com>
2026-05-29 00:47:50 +0300

    separating checkout header component and separting checkout components into a folder

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,7 +1,7 @@
 import {Route, Routes} from 'react-router'
 import './App.css'
 import { HomePage } from './pages/HomePage'
-import { CheckoutPage } from './pages/CheckoutPage'
+import { CheckoutPage } from './pages/checkout/CheckoutPage'
 import { OrdersPage } from './pages/OrdersPage'
 import { TrackingPage } from './pages/TrackingPage'
 
