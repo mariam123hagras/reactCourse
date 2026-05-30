9750e97d966bd2450bc2a6fbe280e2e649bea2a9
Mariam Hagras <mariamhagras99@gmail.com>
2026-05-30 16:53:27 +0300

    fetching data from bckend

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -3,6 +3,13 @@ import { products } from "../../starting-code/data/products";
 import "./HomePage.css";
 import checkMark from "../assets/images/icons/checkmark.png"
 export function HomePage() {
+  fetch('http://localhost:3000/api/products')
+  .then((response)=>{
+   return response.json()
+    }).then((data)=>{
+      console.log(data)}
+    )
+ 
   return (
     <>
       <link rel="icon" type="image/svg+xml" href="images/home-favicon.png" />
