ac8d21f164f23db408b40247f7bde506588c62d0
Mariam Hagras <mariamhagras99@gmail.com>
2026-06-05 00:26:11 +0300

    finishing exercises to exercise f

--- ecommerce-project/src/pages/home/Product.test.jsx
+++ ecommerce-project/src/pages/home/Product.test.jsx
@@ -14,21 +14,22 @@ describe("Product", () => {
   let product;
   //   creating a fake function that doesn't do anything
   let loadCart;
+  let user;
   // beforeEach is used to reset the mocked data before each test case so that the tests are independent and don't affect each other
-  beforeEach(() => {
-    product =
-      {
-        id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-        image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-        name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-        rating: {
-          stars: 4.5,
-          count: 87,
-        },
-        priceCents: 1090,
-        keywords: ["socks", "sports", "apparel"],
-      };
+  beforeEach(async() => {
+    product = {
+      id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+      name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+      rating: {
+        stars: 4.5,
+        count: 87,
+      },
+      priceCents: 1090,
+      keywords: ["socks", "sports", "apparel"],
+    };
     loadCart = vi.fn();
+   user = await userEvent.setup();
   });
   it("display the product details correctly", () => {
     render(<Product product={product} loadCart={loadCart} />);
@@ -48,13 +49,28 @@ describe("Product", () => {
   });
   it("add a product to car", async () => {
     render(<Product product={product} loadCart={loadCart} />);
-    const user = userEvent.setup();
+   
     const addToCartButton = screen.getByTestId("add-to-cart-button");
 
     await user.click(addToCartButton);
     expect(axios.post).toHaveBeenCalledWith("/api/cart-items", {
       productId: product.id,
       quantity: 1,
+    });
+    expect(loadCart).toHaveBeenCalled();
+  });
+  it("select quantity", async () => {
+    render(<Product product={product} loadCart={loadCart} />);
+    const quantitySelector = screen.getByTestId("quantity-select");
+
+    expect(quantitySelector).toHaveValue("1");
+    await user.selectOptions(quantitySelector, "3");
+    expect(quantitySelector).toHaveValue("3");
+    const addToCartButton = screen.getByTestId("add-to-cart-button");
+    await user.click(addToCartButton);
+    expect(axios.post).toHaveBeenCalledWith("/api/cart-items", {
+      productId: product.id,
+      quantity: 3,
     });
     expect(loadCart).toHaveBeenCalled();
   });
